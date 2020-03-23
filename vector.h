1a0cc0ef99efa96ad34b0e4405ffe4d482e08ee6
amelie106 <62565813+amelie106@users.noreply.github.com>
2020-03-23 19:13:09 +0100

    Add files via upload

---
+++ vector.h
@@ -0,0 +1,313 @@
+#ifndef __VECTOR_H__
+#define __VECTOR_H__
+
+#include <iostream>
+#include <initializer_list>
+#include <cstddef>
+
+using namespace std;
+
+template <typename T>
+class Vector {
+
+	public:
+
+		class ConstIterator;
+		class Iterator;
+		using value_type = T;
+		using size_type = size_t;
+		using difference_type = ptrdiff_t;
+		using reference = value_type&;
+		using const_reference = const value_type&;
+		using pointer = value_type*;
+		using const_pointer = const value_type*;
+		using iterator = Vector::Iterator;
+		using const_iterator = Vector::ConstIterator;
+	
+	private:
+
+		size_type sz;
+		size_type max_sz;
+		pointer values;
+		static constexpr size_type min_sz {5};
+
+	public:
+
+		Vector (size_type n = min_sz) {
+			if (n < min_sz)
+				max_sz = min_sz;
+			else
+				max_sz = n;
+						
+			sz = 0;
+			values = new value_type [max_sz];
+		} 
+
+		Vector (initializer_list<value_type> il) {
+			max_sz = il.size();
+			values = new value_type [max_sz];
+			sz = max_sz;
+
+			size_type a {0};
+
+			for (const auto& elem : il) {
+				values [a] = elem;
+				a++;
+			}
+		}
+
+		Vector (Vector& v) {
+			sz = v.sz;
+			max_sz = v.max_sz;
+			
+			values = new value_type [max_sz];
+			values = v.values;
+		}
+
+		~Vector () {
+			delete [] values;
+		}
+
+		iterator begin () {
+			return iterator {values};
+		}
+
+		const_iterator begin () const {
+			return values;
+		}
+
+		iterator end () {
+			return values + sz;
+		}
+
+		const_iterator end () const {
+			return values + sz;
+		}
+
+		iterator insert (const_iterator pos, const_reference val) {
+			auto diff = pos-begin();
+
+			if (diff < 0 || static_cast<size_type>(diff) > sz)
+				throw runtime_error ("Iterator out of bounds");
+
+			size_type current {static_cast<size_type>(diff)};
+
+			if (sz >= max_sz)
+				reserve (max_sz*2+1);
+
+			for (size_type i {sz}; i-- > current;)
+				values[current] = val;
+
+			++sz;
+
+			return iterator {values + current};			
+		} 
+
+		iterator erase (const_iterator pos) {
+			auto diff = pos-begin();
+
+			if (diff < 0 || static_cast<size_type> (diff) >= sz)
+				throw runtime_error("Iterator out of bounds");
+
+			size_type current {static_cast<size_type> (diff)};
+
+			for (size_type i {current}; i < sz-1; ++i)
+				values[i] = values [i+1];
+
+			--sz;
+					
+			return iterator {values + current};
+		}
+
+		value_type at (size_type a) const {
+			return values[a];
+		}
+
+		size_type size () const {
+			return sz;
+		}
+
+		bool empty () const {
+			if (sz == 0)
+				return true;
+			else
+				return false;
+		}
+
+		void clear () {
+			delete [] values;
+			sz = 0;
+		}
+
+		void reserve (size_type n) {
+			if (n > max_sz)
+				max_sz = n;
+		} 
+
+		void shrink_to_fit () {
+			if (sz >= min_sz)
+				max_sz = sz;
+		}
+
+		void push_back (value_type x) {
+			values [++sz] = x;
+		}
+
+		void pop_back () {
+			if (this->empty())
+				throw runtime_error ("Vektor ist leer.");
+
+			sz--;
+		}
+
+		void operator= (const Vector& b) {
+			sz = b.sz;
+			max_sz = b.max_sz;
+			values = b.values;
+		}
+
+		value_type& operator[] (size_type index) {
+			return values[index];
+		}
+
+		const value_type& operator[] (size_type index) const {
+			return values[index];
+		}
+
+		size_type capacity () const {
+			return max_sz;
+		}
+        
+        ostream& print (ostream& o) {
+            o << '[';
+
+        	bool first {true};
+
+	        for (int a {0}; a < sz; a++) {
+		        if (first) {
+			        o << values[a];
+			        first = false;
+		        }
+		        else {
+			        o << ", " << values[a];
+		        }
+	        }
+
+	        o << ']';
+
+        	return o;
+        }
+
+	class Iterator {
+	
+		public:
+
+			using value_type = Vector::value_type;
+			using reference = Vector::reference;
+			using pointer = Vector::pointer;
+			using difference_type = Vector::difference_type;
+			using iterator_category = forward_iterator_tag;
+		
+		private:
+			
+			pointer ptr;
+
+		public:
+
+			Iterator (pointer p = nullptr) : ptr {p} {}
+
+			pointer get_ptr () const {return ptr;};
+			
+			reference operator* () {
+				return *ptr;
+			}
+
+			pointer operator-> () {
+				return ptr;
+			}
+
+			bool operator!= (const const_iterator& other) const {
+				return static_cast<ConstIterator> (*this) != other;
+			}
+
+			bool operator== (const const_iterator& other) const {
+				return static_cast<ConstIterator> (*this) == other;
+			}
+
+			iterator& operator++ () {
+				++ptr;
+ 				return *this;
+			}
+
+			iterator operator++ (int) {
+				iterator temp = *this;
+ 				++(*this);
+  				return temp;
+			}
+
+			operator const_iterator () const {
+				return ConstIterator{ptr};
+			}
+
+		};
+
+	class ConstIterator {
+	    
+	    Vector::difference_type operator-(const ConstIterator& other) const { 
+            return ptr-other.ptr;
+        }
+	    
+		public:
+
+			using value_type = Vector::value_type;
+			using reference = Vector::const_reference;
+			using pointer = Vector::const_pointer;
+			using difference_type = Vector::difference_type;
+			using iterator_category = forward_iterator_tag;
+
+		private:
+				
+			pointer ptr;
+
+		public:
+					
+			ConstIterator (pointer p = nullptr) : ptr {p} {}
+
+			pointer get_ptr () const {return ptr;};
+
+			bool operator== (const const_iterator& other) const {
+				return ptr == other.ptr;
+			}
+
+			bool operator!= (const const_iterator& other) const {
+				return ptr != other.ptr;
+			}			
+
+			reference operator* () const {
+				return *ptr;
+			}
+
+			pointer operator-> () const {
+				return ptr;
+			}
+
+			const_iterator& operator++ () {
+				return ++(*this);
+			}
+
+			const_iterator operator++ (int) {
+				const_iterator temp = *this;
+ 				++(*this);
+  				return temp;
+			}
+
+	};
+
+};
+
+template <typename T>
+ostream& operator<< (ostream& o, Vector<T> v) {
+	return v.print(o);
+}
+
+#endif
